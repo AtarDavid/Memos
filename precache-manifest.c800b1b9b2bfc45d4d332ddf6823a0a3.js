b56741026278454f595c4c7e1790a90215bf3fc1
AD <atar.dci@gmail.com>
2019-04-03 14:12:16 +0200

    Updates

---
+++ precache-manifest.c800b1b9b2bfc45d4d332ddf6823a0a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "34445ca904c167686b18",
+    "url": "./static/js/main.0208406d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "./static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "34445ca904c167686b18",
+    "url": "./static/css/main.d4d24a8c.chunk.css"
+  },
+  {
+    "revision": "e92befc0c3b6139f154cd38e82be58fa",
+    "url": "./index.html"
+  }
+];
